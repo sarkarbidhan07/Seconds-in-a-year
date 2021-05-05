ccd856a09611e3716ae8fe4112149e2bb88ba946
sarkarbidhan07 <57905138+sarkarbidhan07@users.noreply.github.com>
2021-05-05 10:45:58 +0600

    Add files via upload

---
+++ seconds_per_year.py
@@ -0,0 +1,11 @@
+def main():
+    days_a_year =365
+    hours_a_day=24
+    minutes_an_hour=60
+    seconds_per_minute=60
+    Seconds_ina_year=days_a_year * hours_a_day* minutes_an_hour*seconds_per_minute
+    print("Seconds in a year:"  ,Seconds_ina_year)
+
+
+if __name__ == "__main__":
+    main()
